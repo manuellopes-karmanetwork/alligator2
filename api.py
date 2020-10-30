4517afe12c0b49e8a5e283090d2ae01d31f54d63
manuellopes-karmanetwork <72551714+manuellopes-karmanetwork@users.noreply.github.com>
2020-10-30 12:39:36 +0000

    Update api.py

--- api.py
+++ api.py
@@ -38,7 +38,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/bigquery",
     "https://www.googleapis.com/auth/cloud-language"
 ]
-DATASET_ID = "anf"
+DATASET_ID = "alligator_karma"
 MAX_RETRIES = 10
 MIN_TOKENS = 20
 INSIGHTS_DAYS_BACK = 540
